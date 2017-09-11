c524dcce0938fc2190d0f6299755eb46acfa4a97
divanovg94 <divanovg94@gmail.com>
2017-09-11 10:31:12 +0300

    admin panel and some view added

--- myApp/db/schema.rb
+++ myApp/db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170907121735) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
     t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
